7d33176beaaf7713a1c89a31a384689950472770
PRESISPREMACHANDRAN <presipremachandran@gmail.com>
2024-01-06 17:42:04 +0530

    value read from form

--- src/Components/AddPatient.jsx
+++ src/Components/AddPatient.jsx
@@ -1,69 +1,171 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const AddPatient = () => {
+
+  const [input, changeInput] = useState({
+    uhidNo: "",
+    patientName: "",
+    hospitalName: "",
+    doctorName: "",
+    gender: "",
+    dob: "",
+    email_id:"",
+    address:"",
+    mobileNo:"",
+    pin_code:""
+  });
+
+const readValue=(e)=>[
+  changeInput({...input,[e.target.name]:e.target.value})
+]
+
+const submitValue=()=>{
+  console.log(input)
+}
+
+
   return (
     <div>
-      <Header/>
-        
-        <div className="container">
-            <div className="row">
-                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+      <Header />
 
-                    <div className="row g-3">
-                      <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-                        <label htmlFor="" className="form-label">Patient Name</label>
-                        <input type="text" className="form-control" />
-                      </div>
-                      <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-                        <label htmlFor="" className="form-label">Gender</label>
-                        <input type="text" className="form-control" />
-                      </div>
-                      <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-                        <label htmlFor="" className="form-label">UHID Number</label>
-                        <input type="text" className="form-control" />
-                      </div>
-                      <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-                        <label htmlFor="" className="form-label">Hospital Name</label>
-                        <input type="text" className="form-control" />
-                      </div>
-                      <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-                        <label htmlFor="" className="form-label">Date of Birth</label>
-                        <input type="date" name="" id="" className="form-control" />
-                      </div>
-                      <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-                        <label htmlFor="" className="form-label">Doctor Name</label>
-                        <input type="text" className="form-control" />
-                      </div>
-                      <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-                        <label htmlFor="" className="form-label">Mobile Number</label>
-                        <input type="text" className="form-control" />
-                      </div>
-                      <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-                        <label htmlFor="" className="form-label">Email ID</label>
-                        <input type="email" name="" id="" className="form-control" />
-                      </div>
-                      <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-                        <label htmlFor="" className="form-label">Address</label>
-                        <input type="text" className="form-control" />
-                        
-                      </div>
-                      <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-                        <label htmlFor="" className="form-label">Pin Code</label>
-                        <input type="text" className="form-control" />
-                      </div>
-                      <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <button className="btn btn-success">Submit</button>
-                      </div>
-                    </div>
-                </div>
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <label htmlFor="" className="form-label">
+                  Patient Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="patientName"
+                  value={input.patientName}
+                  onChange={readValue}
+                />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <label htmlFor="" className="form-label">
+                  Gender
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="gender"
+                  value={input.gender}
+                  onChange={readValue}
+                />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <label htmlFor="" className="form-label">
+                  UHID Number
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="uhidNo"
+                  value={input.uhidNo}
+                  onChange={readValue}
+                />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <label htmlFor="" className="form-label">
+                  Hospital Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="hospitalName"
+                  value={input.hospitalName}
+                  onChange={readValue}
+                />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <label htmlFor="" className="form-label">
+                  Date of Birth
+                </label>
+                <input
+                  type="date"
+                  name=""
+                  id=""
+                  className="form-control"
+                  name="dob"
+                  value={input.dob}
+                  onChange={readValue}
+                />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <label htmlFor="" className="form-label">
+                  Doctor Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="doctorName"
+                  value={input.doctorName}
+                  onChange={readValue}
+                />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <label htmlFor="" className="form-label">
+                  Mobile Number
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="mobileNo"
+                  value={input.mobileNo}
+                  onChange={readValue}
+                />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <label htmlFor="" className="form-label">
+                  Email ID
+                </label>
+                <input
+                  type="email"
+                  name=""
+                  id=""
+                  className="form-control"
+                  name="email_id"
+                  value={input.email_id}
+                  onChange={readValue}
+                />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <label htmlFor="" className="form-label">
+                  Address
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="address"
+                  value={input.address}
+                  onChange={readValue}
+                />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <label htmlFor="" className="form-label">
+                  Pin Code
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="pin_code"
+                  value={input.pin_code}
+                  onChange={readValue}
+                />
+              </div>
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <button className="btn btn-success" onClick={submitValue}>Submit</button>
+              </div>
             </div>
+          </div>
         </div>
-
-
-
+      </div>
     </div>
-  )
+  );
 }
 
 export default AddPatient
--- src/Components/DeletePatient.jsx
+++ src/Components/DeletePatient.jsx
@@ -1,31 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const DeletePatient = () => {
+
+    const [input, changeInput] = useState({
+      uhidNo: ""
+    });
+
+    const readValue = (e) => [
+      changeInput({ ...input, [e.target.name]: e.target.value }),
+    ];
+
+    const submitValue = () => {
+      console.log(input);
+    };
   return (
     <div>
-      <Header/>
+      <Header />
 
-<div className="container">
-  <div className="row">
-    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-
-      <div className="row g-3">
-        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-          <label htmlFor="" className="form-label">UHID Number</label>
-          <input type="text" className="form-control" />
-        </div>
-        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-          <button className="btn btn-success">Delete</button>
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="" className="form-label">
+                  UHID Number
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="uhidNo"
+                  value={input.uhidNo}
+                  onChange={readValue}
+                />
+              </div>
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <button className="btn btn-success" onClick={submitValue}>Delete</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
     </div>
-  </div>
-</div>
-
-    </div>
-  )
+  );
 }
 
 export default DeletePatient
